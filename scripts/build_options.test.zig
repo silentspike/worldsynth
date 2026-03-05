20c871e21ce7a65070c45e106aa905c90e545f26
obtFusi <obtFusi@users.noreply.github.com>
2026-03-05 10:23:11 +0100

    ci(benchmark): speed up main/sprint benchmark workflow

--- scripts/build_options.test.zig
+++ scripts/build_options.test.zig
@@ -2,4 +2,5 @@ pub const enable_cuda = false;
 pub const enable_tensorrt = false;
 pub const enable_pipewire = false;
 pub const enable_jack = false;
+pub const enable_alsa = false;
 pub const enable_neural = false;
